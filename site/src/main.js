9397378e59c287815f86d9492dacfe32596678d7
huangwenfu750 <142565498+huangwenfu750@users.noreply.github.com>
2026-09-25 13:37:08 +0800

    Delete site directory

--- site/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { applyDocumentLang } from './i18n/index.js'
-import { applyTheme } from './theme.js'
-import './styles.css'
-
-applyTheme()
-applyDocumentLang()
-createApp(App).mount('#app')
